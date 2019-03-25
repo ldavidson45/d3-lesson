724248ba9d5a12e86f59145456d343a68b4b06ac
Liz Davidson <lizdavidson45@gmail.com>
2019-03-25 14:49:14 -0400

    add data

---
+++ script.js
@@ -0,0 +1,46 @@
+var dataSet = [
+  {
+    candidate: "Kamala Harris",
+    endorsementsCount: 21
+  },
+  {
+    candidate: "Joe Biden",
+    endorsementsCount: 3
+  },
+  {
+    candidate: "Julian Castro",
+    endorsementsCount: 5
+  },
+  {
+    candidate: "Bernie Sanders",
+    endorsementsCount: 8
+  },
+  {
+    candidate: "Cory Booker",
+    endorsementsCount: 18
+  },
+  {
+    candidate: "Elizabeth Warren",
+    endorsementsCount: 5
+  },
+  {
+    candidate: "John Delaney",
+    endorsementsCount: 2
+  },
+  {
+    candidate: "Beto O'Rourke",
+    endorsementsCount: 5
+  },
+  {
+    candidate: "Jay Inslee",
+    endorsementsCount: 2
+  },
+  {
+    candidate: "Amy Klobuchar",
+    endorsementsCount: 10
+  },
+  {
+    candidate: "John Hickenlooper",
+    endorsementsCount: 1
+  }
+];
