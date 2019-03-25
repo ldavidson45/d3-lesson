47149088b805ac25e498ca8545e166aad8dea8da
Liz Davidson <lizdavidson45@gmail.com>
2019-03-25 15:06:45 -0400

    render svg chart

--- script.js
+++ script.js
@@ -53,3 +53,21 @@ const svg = d3
 
 const barPadding = 5;
 const barWidth = 200 / dataSet.length;
+
+// create bar chart by appending svg shapes into the element. Define the placement and attributes for each data item
+const barChart = svg
+  .selectAll("rect")
+  .data(dataSet)
+  .enter()
+  .append("rect")
+  .attr("y", d => {
+    return 300 - d.endorsementsCount;
+  })
+  .attr("height", d => {
+    return d.endorsementsCount;
+  })
+  .attr("width", barWidth - barPadding)
+  .attr("transform", (d, i) => {
+    var translate = [barWidth * i, 0];
+    return `translate(${translate})`;
+  });
